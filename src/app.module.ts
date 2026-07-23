5fd2c619f7280f76b011507b60bf03a277460187
Adel Ahmed <adel15952@gmail.com>
2026-07-23 20:05:21 +0300

    Add Users module with controller, service, and DTOs; integrate @nestjs/mapped-types for update functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { MongooseModule } from '@nestjs/mongoose';
         uri: configService.getOrThrow<string>('MONGODB_URI'),
       }),
     }),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
