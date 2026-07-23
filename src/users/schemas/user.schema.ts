5c8791da481d286b917653e3fb0e228bb378cea7
Adel Ahmed <adel15952@gmail.com>
2026-07-24 00:16:07 +0300

    Add UserRole enum and enhance User schema with additional properties and validations

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,12 +1,57 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
+import { UserRole } from '../enums/user.enum';
 
 export type UserDocument = HydratedDocument<User>;
 
 @Schema({ timestamps: true })
 export class User {
-  @Prop()
+  @Prop({
+    required: true,
+    type: String,
+    min: [3, 'Name must be at least 3 characters long'],
+    max: [30, 'Name must be less than 30 characters long'],
+  })
   name: string;
+
+  @Prop({
+    required: true,
+    type: String,
+    unique: true,
+  })
+  email: string;
+
+  @Prop({
+    required: true,
+    type: String,
+    min: [3, 'Password must be at least 8 characters long'],
+    max: [20, 'Password must be less than 20 characters long'],
+  })
+  password: string;
+
+  @Prop({
+    required: true,
+    type: String,
+    enum: UserRole,
+    default: UserRole.USER,
+  })
+  role: UserRole;
+
+  @Prop({
+    type: String,
+    default: null,
+  })
+  avatar: string | null;
+
+  @Prop({
+    type: Number,
+  })
+  age: number;
+
+  @Prop({
+    type: String,
+  })
+  phoneNumber: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
