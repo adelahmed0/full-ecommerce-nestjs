9f28382454751f063cd6b7a944b7b0f01449abe5
Adel Ahmed <adel15952@gmail.com>
2026-07-23 19:52:25 +0300

    Add .env.example file, integrate @nestjs/config for environment variables, and update package dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.getOrThrow<string>('MONGODB_URI'),
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
