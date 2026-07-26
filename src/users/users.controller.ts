3dd75fd28a9629772c41cad005ed31d94330c18f
Adel Ahmed <adel15952@gmail.com>
2026-07-27 00:11:19 +0300

    Add response messages to user-related endpoints in UsersController, enhancing user feedback for create, fetch, update, and delete operations.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,32 +10,38 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ResponseMessage } from '../common/decorators/response-message.decorator';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ResponseMessage('User created successfully')
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @ResponseMessage('Users fetched successfully')
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @ResponseMessage('User fetched successfully')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
   @Patch(':id')
+  @ResponseMessage('User updated successfully')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
+  @ResponseMessage('User deleted successfully')
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
