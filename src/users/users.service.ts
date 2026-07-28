1d6792735eafe90f21372c07d1ad99ccbd35c2c3
Adel Ahmed <adel15952@gmail.com>
2026-07-28 23:22:39 +0300

    Refactor user creation error handling in UsersService to use ConflictException for existing users, improving clarity and response structure.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,11 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import * as bcrypt from 'bcrypt';
 import { Model } from 'mongoose';
+import { ApiMessage } from '../common/enums/api-message.enum';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './schemas/user.schema';
-import { ApiMessage } from '../common/enums/api-message.enum';
 
 @Injectable()
 export class UsersService {
@@ -14,8 +14,12 @@ export class UsersService {
   async create(createUserDto: CreateUserDto) {
     const user = await this.userModel.findOne({ email: createUserDto.email });
     if (user) {
-      throw new BadRequestException(ApiMessage.USER_ALREADY_EXISTS);
+      throw new ConflictException({
+        message: ApiMessage.USER_ALREADY_EXISTS,
+        errors: { email: 'Email is already taken' },
+      });
     }
+
     const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
 
     return this.userModel.create({
@@ -32,7 +36,8 @@ export class UsersService {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, _updateUserDto: UpdateUserDto) {
+  update(id: number, updateUserDto: UpdateUserDto) {
+    void updateUserDto;
     return `This action updates a #${id} user`;
   }
 
