54fa1c72b5cf267bd8c49b27c87936ad1b7fc4bf
Adel Ahmed <adel15952@gmail.com>
2026-08-03 19:34:53 +0300

    Add ChangePasswordDto for password change validation and update ApiMessage enum with success message for password change

--- src/common/enums/api-message.enum.ts
+++ src/common/enums/api-message.enum.ts
@@ -16,6 +16,7 @@ export enum ApiMessage {
   USER_PROFILE_FETCHED = 'Profile fetched successfully',
   USER_PROFILE_UPDATED = 'Profile updated successfully',
   USER_PROFILE_DELETED = 'Profile deleted successfully',
+  PASSWORD_CHANGED = 'Password changed successfully',
   USER_UPDATED = 'User updated successfully',
   USER_DELETED = 'User deleted successfully',
 }
